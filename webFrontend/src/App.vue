abf600e56820eba1f36468cb2f4fcbddfe15d14d
Auryn Engel <aengel@itemis.de>
2019-01-19 16:04:01 +0100

    fist component

--- webFrontend/src/App.vue
+++ webFrontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/icons8-blockchain-96.png">
     <router-view/>
   </div>
 </template>
---
+++ webFrontend/src/components/TicketList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ticketListContainer">
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs10 offset-xs1 v-for="ticket in tickets" :key="ticket.name">
+        <v-card light color="primary">
+            <v-card-text class="px-0">
+              {{ticket.name}}
+                <v-btn flat icon color="blue">
+                    <v-icon>send</v-icon>
+                </v-btn>
+            </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TicketList',
+  data () {
+    return {
+      tickets: [{name: 'ticketName'},{name: 'ticketName1'},{name: 'ticketName2'},{name: 'ticketName3'}]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
