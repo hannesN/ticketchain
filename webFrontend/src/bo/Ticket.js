aff7e9103564c1731eb71bc2a2ee935cd395a133
Auryn Engel <aengel@itemis.de>
2019-01-19 16:59:21 +0100

    add ticktes and some blank js files

---
+++ webFrontend/src/bo/Ticket.js
@@ -0,0 +1,8 @@
+class Ticket {
+  constructor (name, id) {
+    this.name = name
+    this.id = id
+  }
+}
+
+export default Ticket
--- webFrontend/src/components/TicketList.vue
+++ webFrontend/src/components/TicketList.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="ticketListContainer">
-  <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs10 offset-xs1 v-for="ticket in tickets" :key="ticket.name">
-        <v-card light color="primary">
-            <v-card-text class="px-0">
-              {{ticket.name}}
-                <v-btn flat icon color="blue">
-                    <v-icon>send</v-icon>
-                </v-btn>
-            </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs12 sm12 md10 offset-md1 v-for="ticket in tickets" :key="ticket.id">
+          <v-card light color="primary">
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{ticket.name}}</div>
+                <span class="grey--text">The Ticket has the ID: {{ticket.id}}</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat icon color="blue">
+                <v-icon>send</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'TicketList',
-  data () {
-    return {
-      tickets: [{name: 'ticketName'},{name: 'ticketName1'},{name: 'ticketName2'},{name: 'ticketName3'}]
+/* eslint-disable */
+  import Ticket from '../bo/Ticket.js'
+  export default {
+    name: 'TicketList',
+    data() {
+      return {
+        tickets: [new Ticket('Konzert Leipzig', '1'),
+          new Ticket('Konzert Berlin', '2')
+        ]
+      }
     }
   }
-}
 </script>
 
 <style scoped>
+  
 </style>
